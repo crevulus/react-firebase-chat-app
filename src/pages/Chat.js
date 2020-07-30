4265a2fdd569c833f2a5d0629ac89cf021d7123e
crevulus <crevulus@gmail.com>
2020-07-30 14:43:51 +0200

    FB config; react routing

---
+++ src/pages/Chat.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Chat extends Component {
+  render() {
+    return <div>this is chat.</div>;
+  }
+}
+
+export default Chat;
---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: "https://chat-82124.firebaseio.com",
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();
